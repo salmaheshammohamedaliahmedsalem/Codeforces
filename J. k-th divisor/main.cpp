891e13f62b296249ba63f0760c3691724d372414
salmaheshammohamedaliahmedsalem <141532319+salmaheshammohamedaliahmedsalem@users.noreply.github.com>
2023-08-06 03:34:54 +0300

    Add files via upload

---
+++ J. k-th divisor/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n,k;
+    cin>>n>>k;
+    long long yes;
+    vector <long long> ans;
+
+    for(int i=1;i*i<=n;i++)
+    {
+        if(ans.size()>=k+1)
+            break;
+        if(n%i==0)
+        {
+            ans.push_back(i);
+            if((n/i)!=i)
+            {
+                ans.push_back(n/i);
+            }
+        }
+    }
+    sort(ans.begin(),ans.end());
+    if(ans.size()>=k)
+    cout<<ans[k-1];
+    else
+        cout<<"-1";
+}
